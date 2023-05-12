5240da457efedd3576cb0ff9b6f1204b9e5e2083
John DH. Park <john.dh.park@icloud.com>
2023-05-12 06:42:34 -0700

    Added minimum functionality of displaying the trip lists for each store when name of the stores are clicked; Also added similar functionality for display the cart items for each trip when a trip is clicked.

--- main.js
+++ main.js
@@ -1,19 +1,48 @@
 document.addEventListener("DOMContentLoaded", async () => {
-	// First, get the reference to the html ul element with the id "stores"
-	const ul = document.querySelector("ul#stores");
+	// First, get the references to 3 ul lists.
+	const storeContainer = document.querySelector("ul#store-list");
+	const tripContainer = document.querySelector("ul#trip-list");
+	const cartContainer = document.querySelector("ul#cart-list");
 
 	// Then, make a fetch request to http://localhost:3000/stores.
 	const res = await fetch("http://localhost:3000/stores");
 	const stores = await res.json();
 
-	// Iterate through each store and create an list item element
-	const listItems = stores.map(({ name }) => {
-		const li = document.createElement("li");
-		li.textContent = name;
+	// Iterate through each store and create an list item element to display
+	const listItems = stores.map(({ id, name, trips }) => {
+		const storeEl = document.createElement("li");
+		storeEl.textContent = name;
 
-		return li;
+		// Clicking on the store name will display the list of trips
+		storeEl.addEventListener("click", (e) => {
+			// Iterate through each store trip and create a list element
+			const storeTrips = trips.map(({ date, items, total_spent }) => {
+				const tripEl = document.createElement("li");
+				tripEl.textContent = date;
+
+				tripEl.addEventListener("click", (e) => {
+					const cartItems = items.map(({ name }) => {
+						const cartEl = document.createElement("li");
+
+						cartEl.textContent = name;
+
+						return cartEl;
+					});
+
+					// Update the cart list
+					cartContainer.replaceChildren(...cartItems);
+				});
+
+				return tripEl;
+			});
+
+			// Update the trip list
+			tripContainer.replaceChildren(...storeTrips);
+		});
+
+		return storeEl;
 	});
 
 	// Append the stores to ul
-	ul.append(...listItems);
+	storeContainer.append(...listItems);
 });
